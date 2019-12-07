aecf3b321602df12a3b069b3bb72e1e58ceaad9b
sascharissling <sascha.rissling@gmail.com>
2019-12-07 08:58:32 +0100

    Install packages and import fonts for development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GlobalStyles from "./GlobalStyles";
+import styled from "@emotion/styled";
+
+//STYLE start
+
+const AppContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+//STYLE end
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <GlobalStyles />
+    </AppContainer>
   );
 }
 
---
+++ src/GlobalStyles.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Global, css } from "@emotion/core";
+export default function GlobalStyles() {
+  return (
+    <Global
+      styles={css`
+        *,
+        *::after,
+        *::before {
+          box-sizing: border-box;
+        }
+
+        body {
+          margin: 0;
+          font-family: "Source Sans Pro", sans-serif;
+          background: #0000ff;
+        }
+      `}
+    />
+  );
+}
+
+//FONTS
+// font-family: 'Abril Fatface', cursive;
+// font-family: 'Source Sans Pro', sans-serif;
