0a261aa4ec48101717d931c1ef2cdab51fd67917
sascharissling <sascha.rissling@gmail.com>
2019-12-07 15:14:29 +0100

    Add imprint

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 //STYLE imports
 import GlobalStyles from "./GlobalStyles";
 import styled from "@emotion/styled";
+import { ThemeProvider } from "emotion-theming";
+import theme from "./utils/theme";
 
 //COMPONENTS import
 import Footer from "./components/Footer";
@@ -26,12 +28,14 @@ const AppContainer = styled.div`
 
 function App() {
   return (
-    <AppContainer>
-      <GlobalStyles />
-      <PageMain />
-      <Footer />
-      <ImpressumLink />
-    </AppContainer>
+    <ThemeProvider theme={theme}>
+      <AppContainer>
+        <GlobalStyles />
+        <PageMain />
+        <Footer />
+        <ImpressumLink />
+      </AppContainer>
+    </ThemeProvider>
   );
 }
 
---
+++ src/utils/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+  primary: "#9932CC",
+  secondary: "#343434",
+  tertiary: "lime",
+  background: "#f7f8f7"
+};
+
+export default theme;
