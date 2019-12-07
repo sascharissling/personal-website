7d1d537748c7f7b88f0ac9e1e28f4c8a5b4c08c6
sascharissling <sascha.rissling@gmail.com>
2019-12-07 10:00:15 +0100

    Add react-popup package

--- src/components/PageMain.js
+++ src/components/PageMain.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Popup from "reactjs-popup";
+
+//STYLE imports
 import styled from "@emotion/styled";
 
 //STYLE start
@@ -16,6 +19,11 @@ const Headline = styled.h1`
   font-size: 3rem;
   color: #343434;
   line-height: 1rem;
+  cursor: pointer;
+  &:hover {
+    color: #9932cc;
+    text-shadow: 3px 9px 114px -36px rgba(0, 0, 0, 1);
+  }
 `;
 
 //STYLE end
@@ -23,7 +31,9 @@ const Headline = styled.h1`
 export default function PageMain() {
   return (
     <PageIdentity>
-      <Headline>Sascha Rissling</Headline>
+      <Popup trigger={<Headline>Sascha Rissling</Headline>} position="center">
+        <img src="/assets/sascharissling.png" alt="Sascha Rissling" />
+      </Popup>
       <p>Web Developer & Music Producer</p>
     </PageIdentity>
   );
