f6ae54ed61ff4cc42cb49609e8ecd9e36ea525ff
Sascha Rissling <53901545+sascharissling@users.noreply.github.com>
2019-12-15 11:15:24 +0100

    Make "Impressum" center on iOS devices (#6)

--- src/components/Legal.js
+++ src/components/Legal.js
@@ -6,11 +6,17 @@ import styled from "@emotion/styled";
 //STYLE start
 
 const LegalBar = styled.div`
-  position: absolute;
-  bottom: 15px;
+  @media (min-width: 600px) {
+    position: absolute;
+    bottom: 15px;
+  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ImpressumLink = styled(Link)`
+  -webkit-appearance: none;
   text-decoration: none;
   color: ${props => props.theme.secondary};
   opacity: 50%;
