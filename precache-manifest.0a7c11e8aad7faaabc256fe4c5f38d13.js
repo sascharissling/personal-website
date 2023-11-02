9cc9ed83d7687e2b4b8c82651cacf3bbca06edf5
Sascha Rissling <53901545+sascharissling@users.noreply.github.com>
2023-11-02 07:59:28 +0100

    Updates

---
+++ precache-manifest.0a7c11e8aad7faaabc256fe4c5f38d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca015d887c6f3591e62b1010a2bdd0d",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "ec81c5c3b01320e9465e",
+    "url": "/personal-website/static/js/2.cc9046f5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/personal-website/static/js/2.cc9046f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a86497bc7b6f2b07723",
+    "url": "/personal-website/static/js/main.00bb42ba.chunk.js"
+  },
+  {
+    "revision": "117e3e5d63e1498f83cb",
+    "url": "/personal-website/static/js/runtime-main.c71e6253.js"
+  },
+  {
+    "revision": "d08b9501304dee260f6e0884eba03af7",
+    "url": "/personal-website/static/media/sascha.d08b9501.jpg"
+  }
+]);
